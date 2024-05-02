2e18a004e9ece2261bf48985429c95e3a5fb44c8
Carlos Henrique <carloshpds@gmail.com>
2024-05-02 00:02:50 -0300

    test: more jest context

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,14 @@
  * For a detailed explanation regarding each configuration property, visit:
  */
 
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
+  testEnvironment: "node",
   clearMocks: true,
   coverageProvider: "v8",
   preset: 'ts-jest',
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "/src/apps/legacy/"],
 };
 
