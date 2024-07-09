6ab705e6aaf5c044de8322f07734fcb4252cf95f
Carlos Henrique <carloshpds@gmail.com>
2024-07-09 19:57:54 -0300

    build(deps): remove ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -8,13 +8,9 @@ const config: Config = {
   testEnvironment: "node",
   clearMocks: true,
   coverageProvider: "v8",
-  preset: 'ts-jest/presets/js-with-ts-esm',
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js)$",
   testPathIgnorePatterns: ["/node_modules/", "/src/apps/legacy/", ".d.ts"],
   reporters: ['default'],
-  transform: {
-    '^.+\\.(ts|js)$': ['ts-jest', { useESM: true }],
-  },
 };
 
 export default config;
