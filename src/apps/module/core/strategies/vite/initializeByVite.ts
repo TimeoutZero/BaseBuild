b0036b6bdc67bcd3ed56e6960fbfc677fa7d6d12
Carlos Henrique <carloshpds@gmail.com>
2024-07-16 23:08:21 -0300

    refactor(vite strategy): split getDefaultBasebuildViteConfig and mergeViteConfigs

--- src/apps/module/core/strategies/vite/initializeByVite.ts
+++ src/apps/module/core/strategies/vite/initializeByVite.ts
@@ -1,9 +1,9 @@
 import { UserConfigFnObject, defineConfig } from "vite"
-import buildBasebuildViteConfigFunction from "./vite.config.js"
+import { mergeViteConfigs } from "./merge.js"
 import { BasebuildCoreConfigs } from "../../../core/types.js"
 
 const initializeByVite = (configs: BasebuildCoreConfigs): UserConfigFnObject => {
-  const bbViteConfigFunction = buildBasebuildViteConfigFunction(configs)
+  const bbViteConfigFunction = mergeViteConfigs(configs)
   const finalViteConfig = defineConfig(bbViteConfigFunction)
   return finalViteConfig
 }
